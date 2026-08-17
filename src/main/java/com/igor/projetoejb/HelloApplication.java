88ba94784f82787fe185d4b71c04b53cea897fa1
igord <igordf2502@gmail.com>
2026-08-17 13:53:43 -0300

    configuração ambiente glassfish

---
+++ src/main/java/com/igor/projetoejb/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.igor.projetoejb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
